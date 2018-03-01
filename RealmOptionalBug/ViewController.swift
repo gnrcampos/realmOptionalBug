0405c8747513b0498bea4a4fde2d0f12785c7a08
Gustavo Sgarbi Campos <gustavo.sgarbi.campos@gmail.com>
2018-03-01 13:11:47 +1100

    Initial Commit

---
+++ RealmOptionalBug/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmOptionalBug
+//
+//  Created by Gustavo Sgarbi Campos on 1/3/18.
+//  Copyright © 2018 Gustavo Sgarbi Campos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
